a54fef8329285274bd4bf70a7264874b8e1e2d6c
miaoshua <945970809@qq.com>
2015-12-23 09:48:41 +0800

    时间操作的封装

--- LogLibrary/timestamp.h
+++ LogLibrary/timestamp.h
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 namespace netlib
 {
@@ -26,47 +27,34 @@ namespace netlib
             
             }
             
-            static struct tm *now()  //获取当前tm格式的时间
+            static struct tm now()  //获取当前tm格式的时间
             {
                 struct timeval tv;
                 struct tm time;
                 gettimeofday(&tv,NULL);    //获取微妙，秒值
-                gmtimer_r(tv.tv_sec,&time);   //将s转换为tm格式
+                gmtime_r(&tv.tv_sec,&time);   //将s转换为tm格式
+                time.tm_year += 1900;
+                return time;
             }
 
-            static timeval *getTime()
+            static timeval getTime()
             {
                 struct timeval tv;
                 gettimeofday(&tv,NULL);
                 return tv;
             }
 
-            static int getTimeDiff(timeval v1,timeval v2)    //获得俩时间的时间差
+            static int getTimeDiff(timeval v1,timeval v2)    //获得俩时间的时间差,返回值为微妙
             {
-                struct timeval t;
-
-                if(v1.tv_usec >= v2.tv_usec)
-                {
-                    t.tv_sec = v1.tv_sec - v2.tv_sec;
-                    t.tv_usec = v1.tv_usec - v2.tv_usec;
-                }
-                else
-                {
-                    t.tv_sec = (v1.tv_sec - 1) - v2.tv_sec;
-                    t.tv_usec = (1000000 + v1.tv_usec) - v2.tv_usec;
-                }
-
-                if(t.tv_sec < 0)
-                {
-                    int temp;
-                    t.tv_sec = t.tv_sec - 2*t.tv_sec;   //转正
-                    temp = t.tv_sec*1000000 - t.tv_usec;
-                    t.tv_usec = temp%1000000;
-                    t.tv_sec = temp/1000000;
-                }
+                int t;
+                t = v1.tv_sec*1000000 + v1.tv_usec - v2.tv_sec*1000000 - v2.tv_usec;
+                t = abs(t);               
+                return t;
             }
 
         private:
             
     };
 }
+
+#endif
