9132873eb6d28d4025d1f76c7ace778d8b218761
miaoshua <945970809@qq.com>
2015-12-27 11:54:42 +0800

    增加了日志关闭及日志等级功能

--- LogLibrary/asynLog.cpp
+++ LogLibrary/asynLog.cpp
@@ -21,34 +21,55 @@ using namespace netlib;
 
 AsynLog::AsynLog(int rollSize)
     :rollSize_(rollSize),
+    running_(true),
     currentBuffer_(new FixBuffer()),
     nextBuffer_(new FixBuffer()),
     acceptThread_(std::bind(&AsynLog::threadFunc,this))
 {
-    
+    printf("日志系统已开启\n");   
 }
 
 AsynLog::~AsynLog()
 {
-
+    stop();
 }
 
-void AsynLog::append(char *logline,int len)
+void AsynLog::setLevel(LogLevel level)
 {
-    std::lock_guard<std::mutex> guard(mutex_);
+    currentLevel_ = level;   
+}
+
+bool AsynLog::aboveLevel(LogLevel level)
+{
+    return level <= currentLevel_;
+}
+
+
+void AsynLog::append(char *logline,int len,LogLevel level)
+{
+    if(aboveLevel(level))   //如果level超过所设等级
+    {
+        std::lock_guard<std::mutex> guard(mutex_);
+        //与时间以及线程id等字符串连接
+        std::string log1(logline);  //日志内容
+        std::string log2(timestamp_.toStringTime());//获得时间戳的字符串
+        char log3[30];
+        snprintf(log3,sizeof(log3),"threadid[%lu]: ",pthread_self());//获得线程id的字符串
+        std::string log4(toStringForLevel(level));  //获得等级对应的字符串
+        log2 = log2 + log3 + log4 + log1;
+        writeBuffer(log2.c_str(),log2.size());  //写入buffer
+    }
+}
+
+
+void AsynLog::writeBuffer(const char *logline,int len)
+{
     if(currentBuffer_->writeableSize() >= len)  //如果当前buffer空间足
     {
-        std::string log1(logline);
-        std::string log2(timestamp_.toStringTime());
-        char log3[30];
-        snprintf(log3,sizeof(log3),"threadid[%lu]: ",pthread_self());
-        log2 = log2 + log3 + log1;
-        currentBuffer_->append(log2.c_str(),log2.size());
-        printf("111\n");
+        currentBuffer_->append(logline,len);
     }
     else
     {
-        printf("222\n");
         buffers_.push_back(std::move(currentBuffer_)); //返回指针，自己变为空
         
         if(nextBuffer_)
@@ -65,9 +86,37 @@ void AsynLog::append(char *logline,int len)
     }
 }
 
-void AsynLog::start(void)
+std::string AsynLog::toStringForLevel(LogLevel level)
 {
+    switch(level)
+    {
+        case LogLevel::OFF:
+            return std::string("OFF ");break;
+        case LogLevel::FATAL:
+            return std::string("FATAL ");break;
+        case LogLevel::ERROE:
+            return std::string("ERROE ");break;
+        case LogLevel::WARN:
+            return std::string("WARN ");break;
+        case LogLevel::INFO:
+            return std::string("INFO ");break;
+        case LogLevel::DEBUG:
+            return std::string("DEBUG ");break;
+        case LogLevel::ALL:
+            return std::string("ALL ");break;
+        default:return std::string("UNKNOWN ");
+    }
+    return NULL;
+}
 
+void AsynLog::stop(void)
+{
+    if(running_)
+    {
+        running_ = false;
+        acceptThread_.join();   //等待后台进程退出
+        printf("日志系统已关闭\n");
+    }
 }
 
 void AsynLog::threadFunc(void)
@@ -77,17 +126,14 @@ void AsynLog::threadFunc(void)
     std::vector<std::unique_ptr<FixBuffer>> buffersToWrite;
     LogFile output(1000);
     buffersToWrite.reserve(16);
-    printf("333\n");
-    while(1)
+    while(running_)
     {
         {
-            printf("444\n");
             std::lock_guard<std::mutex> guard(mutex_);  //临界区加锁
             if(buffers_.empty())    //如果buffers_为空
             {
                 condition_.wait_for(mutex_,std::chrono::seconds(3));
             }
-            printf("555\n");
             buffers_.push_back(std::move(currentBuffer_));
             currentBuffer_ = std::move(newBuffer1);
             buffersToWrite.swap(buffers_);  //交换俩个容器
@@ -97,7 +143,6 @@ void AsynLog::threadFunc(void)
             }
             
         }
-        printf("666\n");
         assert(!buffersToWrite.empty());
 
         if(buffersToWrite.size() > 25)
@@ -108,7 +153,6 @@ void AsynLog::threadFunc(void)
         //将buffer中的内容写进文件中
         for(int i = 0; i < buffersToWrite.size(); i++)
         {
-            printf("777\n");
             output.append(buffersToWrite[i]->getReadPeek(),buffersToWrite[i]->readableSize());        
         }
 
@@ -119,7 +163,6 @@ void AsynLog::threadFunc(void)
 
         if(!newBuffer1)
         {
-            printf("888\n");
             assert(!buffersToWrite.empty());
             newBuffer1 = std::move(buffersToWrite[0]);
             newBuffer1->resetBuffer();  //重置buffer
@@ -127,13 +170,11 @@ void AsynLog::threadFunc(void)
 
         if(!newBuffer2)
         {
-            printf("999\n");
             assert(!buffersToWrite.empty());
             newBuffer2 = std::move(buffersToWrite[1]);
             newBuffer2->resetBuffer();
         }
 
         buffersToWrite.clear();
-        printf("000\n");
     }   
 }
